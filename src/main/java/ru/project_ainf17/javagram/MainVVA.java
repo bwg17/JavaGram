5c66dbf91911e37fa0bfbdca59f3f3b6e3ad83c5
sparrow84 <vorobey280184@mail.ru>
2018-01-05 22:41:28 +0700

    Добавил свой мэйн для тестов

--- src/main/java/ru/project_ainf17/javagram/MainVVA.java
+++ src/main/java/ru/project_ainf17/javagram/MainVVA.java
@@ -3,5 +3,9 @@ package ru.project_ainf17.javagram;
 public class MainVVA {
     public static void main(String[] args) {
         System.out.println("VVA Start ... ");
+
+        for (int i = 0; i < 10; i++)  {
+            System.out.println("Ура " + i);
+        }
     }
 }
