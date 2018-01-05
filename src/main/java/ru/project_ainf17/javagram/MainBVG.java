80300bba3e38d51290738ea10f801c7ceece0401
bwg17 <ainf17@mail.ru>
2018-01-05 18:38:19 +0300

    Добавил End

--- src/main/java/ru/project_ainf17/javagram/MainBVG.java
+++ src/main/java/ru/project_ainf17/javagram/MainBVG.java
@@ -3,5 +3,6 @@ package ru.project_ainf17.javagram;
 public class MainBVG {
     public static void main(String[] args) {
         System.out.println("BVG Start ... ");
+        System.out.println("BVG End ... ");
     }
 }
